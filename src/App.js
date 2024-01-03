4e848fdbe722aa096638670f1c0e45a8baada4dd
mdadnan2023 <adnan.md.viacon@gmail.com>
2024-01-03 17:31:21 +0530

    new1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Search from './components/Search';
 import initialDetails from './data/initialDetails';
 
+
+
 function App() {
   return (
     <div className="tc bg-green ma0 pa4 min-vh-100">
